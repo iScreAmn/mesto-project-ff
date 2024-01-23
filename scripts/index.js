8f51f011a0420d9b7d8a3a91265785f46677395a
Dimitri Jmukhadze <jmukhadze.dimitri@gmail.com>
2024-01-24 00:28:41 +0400

    Code formatting

--- scripts/index.js
+++ scripts/index.js
@@ -11,49 +11,36 @@
 
 
 // Функция для создания элемента карточки
-
 function createCard(name, link) {
   const cardTemplate = document.querySelector('#card-template');
   const cardElement = cardTemplate.content.cloneNode(true);
-
-// Установка значений вложенных элементов
-
+  // Установка значений вложенных элементов
   const cardTitle = cardElement.querySelector('.card__title');
   const cardImage = cardElement.querySelector('.card__image');
-
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
-
-// Добавление обработчика клика для удаления карточки
-
+  // Добавление обработчика клика для удаления карточки
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', handleDeleteCard);
   return cardElement;
 }
-
 // Обработчик события для удаления карточки
-
 function handleDeleteCard(event) {
   const cardElement = event.target.closest('.places__item');
   if (cardElement) {
-    cardElement.remove();
+      cardElement.remove();
   }
 }
-
 // Функция для добавления карточек на страницу
-
 function renderCards(cards) {
   const placesList = document.querySelector('.places__list');
-
   cards.forEach((cardData) => {
-    const cardElement = createCard(cardData.name, cardData.link);
-    placesList.appendChild(cardElement);
+      const cardElement = createCard(cardData.name, cardData.link);
+      placesList.appendChild(cardElement);
   });
 }
-
 // Вызов функции для добавления карточек на страницу при загрузке
-
 renderCards(initialCards);
 
 
