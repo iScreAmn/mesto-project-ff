98a79b6c1b702052d1a9ee3acec3af65c39bc48c
Dimitri Jmukhadze <jmukhadze.dimitri@gmail.com>
2024-01-24 00:22:18 +0400

    Add check-list requirements
    Function for creating a card element
    Setting nested element values
    Adding a click handler to remove a card
    Initiating a function to place cards on the page upon its initial load

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -7,3 +8,53 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+// Функция для создания элемента карточки
+
+function createCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.content.cloneNode(true);
+
+// Установка значений вложенных элементов
+
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+// Добавление обработчика клика для удаления карточки
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', handleDeleteCard);
+  return cardElement;
+}
+
+// Обработчик события для удаления карточки
+
+function handleDeleteCard(event) {
+  const cardElement = event.target.closest('.places__item');
+  if (cardElement) {
+    cardElement.remove();
+  }
+}
+
+// Функция для добавления карточек на страницу
+
+function renderCards(cards) {
+  const placesList = document.querySelector('.places__list');
+
+  cards.forEach((cardData) => {
+    const cardElement = createCard(cardData.name, cardData.link);
+    placesList.appendChild(cardElement);
+  });
+}
+
+// Вызов функции для добавления карточек на страницу при загрузке
+
+renderCards(initialCards);
+
+
+
