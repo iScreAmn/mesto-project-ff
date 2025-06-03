ba559dd02641085ceb8f2106f8bce3a77942ff8f
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-06-03 14:54:30 +0400

    Extracted cards data array into separate component

---
+++ src/components/cards.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+  {
+    name: "Switzerland",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Ukraine",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Serbia",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Norway",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Poland",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Arctic",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  }
+];
